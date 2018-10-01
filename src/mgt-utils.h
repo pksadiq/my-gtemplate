644a2d94bbf2e4a5180d994d275d1a90fcd782f3
Mohammed Sadiq <sadiq@sadiqpk.org>
2018-10-01 20:34:56 +0530

    project: Add Authors list to license blurbs
    Many firms keep the copyright of the code written by their employees.
    So it's better to keep a list of authors for reference.
    
    You may find 'git blame' interesting if you need accurate details
    of who wrote a specific line of code (Please note: As many developers
    make use of this data, it is recommended to not make whiteline
    changes as much as possible).

--- src/mgt-utils.h
+++ src/mgt-utils.h
@@ -15,6 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * Authors:
+ *   Mohammed Sadiq <sadiq@sadiqpk.org>
+ *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
