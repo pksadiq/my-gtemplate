395e2a6a8ad86d4277325f963d83cc02ae906bfe
Mohammed Sadiq <sadiq@sadiqpk.org>
2022-08-20 15:29:46 +0530

    Update copyright year to 2022

--- src/mgt-utils.h
+++ src/mgt-utils.h
@@ -1,7 +1,7 @@
 /* -*- mode: c; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
 /* mgt-utils.h
  *
- * Copyright 2021 Mohammed Sadiq <sadiq@sadiqpk.org>
+ * Copyright 2022 Mohammed Sadiq <sadiq@sadiqpk.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
