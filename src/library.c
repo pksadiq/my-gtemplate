885851615a553935160a9afad4031ce2aed68730
Mohammed Sadiq <sadiq@sadiqpk.org>
2022-08-21 20:17:01 +0530

    tests: Fix validating ui files

---
+++ src/library.c
@@ -0,0 +1,25 @@
+/* -*- mode: c; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+/* library.h
+ *
+ *
+ * Author(s):
+ *   Mohammed Sadiq <sadiq@sadiqpk.org>
+ *
+ * SPDX-License-Identifier: GPL-2-or-later OR CC0-1.0
+ */
+
+#include <adwaita.h>
+
+#include "mgt-window.h"
+
+void __attribute__((constructor)) ensure_types (void);
+
+void
+__attribute__((constructor)) ensure_types (void)
+{
+  gtk_init ();
+  adw_init ();
+
+  /* fixme: Why do gtk warn with the following code? */
+  /* g_type_ensure (MGT_TYPE_WINDOW); */
+}
