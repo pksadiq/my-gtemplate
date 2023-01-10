136a614bcd25b5f343ab7e27eea022c3b3f018d8
Mohammed Sadiq <sadiq@sadiqpk.org>
2023-01-10 10:35:08 +0530

    various: Remove GNU Emacs file variables
    Since GNU Emacs 29, we also have c-ts-mode for C source files,
    so don't enforce c-mode.
    
    Add desired changes in .dir-locals instead.

--- src/mgt-window.c
+++ src/mgt-window.c
@@ -1,4 +1,3 @@
-/* -*- mode: c; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
 /* mgt-window.c
  *
  * Copyright 2023 Mohammed Sadiq <sadiq@sadiqpk.org>
--- tests/utils.c
+++ tests/utils.c
@@ -1,4 +1,3 @@
-/* -*- mode: c; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
 /* utils.c
  *
  * Copyright 2023 Mohammed Sadiq <sadiq@sadiqpk.org>
