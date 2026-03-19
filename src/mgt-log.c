71e9835baaf36ac45b33b682201e6399c481b8ee
Mohammed Sadiq <sadiq@sadiqpk.org>
2026-03-19 16:32:00 +0530

    log: Always log messages
    This is the behavior upstream. Follow the same

--- src/mgt-log.c
+++ src/mgt-log.c
@@ -74,19 +74,16 @@ static gboolean
 should_show_log_for_level (GLogLevelFlags log_level,
                            int            verbosity_level)
 {
-  if (verbosity_level >= 5)
+  if (verbosity_level >= 6)
     return TRUE;
 
   if (log_level & MGT_LOG_LEVEL_TRACE)
-    return verbosity_level >= 4;
-
-  if (log_level & G_LOG_LEVEL_DEBUG)
     return verbosity_level >= 3;
 
-  if (log_level & G_LOG_LEVEL_INFO)
+  if (log_level & G_LOG_LEVEL_DEBUG)
     return verbosity_level >= 2;
 
-  if (log_level & G_LOG_LEVEL_MESSAGE)
+  if (log_level & G_LOG_LEVEL_INFO)
     return verbosity_level >= 1;
 
   return FALSE;
@@ -122,13 +119,17 @@ should_log (const char     *log_domain,
   if (log_level & (G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL | G_LOG_LEVEL_WARNING))
     return TRUE;
 
+  /* always log messages - the default behavior of GLib */
+  if (log_level & G_LOG_LEVEL_MESSAGE)
+    return TRUE;
+
   if (any_domain && domains) {
     /* If domain is “all” show logs upto debug regardless of the verbosity */
     if (log_level & ~MGT_LOG_LEVEL_TRACE)
       return TRUE;
 
-    /* If the log is trace level, log if verbosity >= 4 */
-    return verbosity >= 4;
+    /* If the log is trace level, log if verbosity >= 3 */
+    return verbosity >= 3;
   }
 
   if (!domains && g_str_has_prefix (log_domain, DEFAULT_DOMAIN_PREFIX))
@@ -143,12 +144,12 @@ should_log (const char     *log_domain,
     return FALSE;
 
   /* GdkPixbuf logs are too much verbose, skip unless asked not to. */
-  if (verbosity < 8 &&
+  if (verbosity < 7 &&
       g_strcmp0 (log_domain, "GdkPixbuf") == 0 &&
       (!domains || !strstr (domains, log_domain)))
     return FALSE;
 
-  if (verbosity >= 6)
+  if (verbosity >= 5)
     return TRUE;
 
   return FALSE;
